e33212c98b3da50f2aa4e0794a3c0b0324f8e442
Srav <user@Sravs-MacBook.fios-router.home>
2018-07-30 22:59:39 -0400

    consmetic

--- meteors/find-meteor.py
+++ meteors/find-meteor.py
@@ -17,7 +17,7 @@ def calc_dist(lat1, lon1, lat2, lon2):
 def get_dist(meteor):
     return meteor.get('distance', math.inf)
     
-if __name__ == '__main__':
+if __name__ == '__main__': 
     my_loc = (29.424122, -98.493628)
 
     meteor_resp = requests.get('https://data.nasa.gov/resource/y77d-th95.json')
